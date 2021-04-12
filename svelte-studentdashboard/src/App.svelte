973ec2b2c121d52eb25043f4e9faccb0350f4276
DebbySpaninks <dspaninks@gmail.com>
2021-04-12 16:35:24 +0200

    try to fetch studentdata

--- svelte-studentdashboard/src/App.svelte
+++ svelte-studentdashboard/src/App.svelte
@@ -1,41 +1,31 @@
 <script>
-	import { onMount } from 'svelte';
-	import studentdata from '../data/studentdata';
-	export let name;
+  import StudentData from './data/StudentData.svelte';
 
+  let show = true;
 
 // 	const csv = `hallo,1,2,3
 // doei,4,5,6`;
-//
-const parse = str => str.split('\n').map(row => row.split(','));
+// const parse = str => str.split('\n').map(row => row.split(','));
+// console.log(parse(studentdata));
 
-console.log(parse(studentdata));
 
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
+<button on:click={() => (show = !show)}>Toggle</button>
+
+{#if show}
+<StudentData />
+{/if}
 
 <style>
-	main {
+/*
+main {
+	display: flex;
+	flex-direction: column;
+	align-items: flex-start;
+} */
+	/* h1 {
+		font-size: 72px;
 		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
-
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+	} */
 </style>
---
+++ svelte-studentdashboard/src/data/StudentData.svelte
@@ -0,0 +1,12 @@
+<script>
+import { onMount } from 'svelte';
+
+let studentdata = [];
+
+onMount(async() => {
+  console.log('Mounted');
+
+  const response = await fetch('/studentdata.csv');
+  studentdata = await response.text();
+});
+</script>
