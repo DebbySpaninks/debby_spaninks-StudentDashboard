f0625a26f7c84b8bc0ac6dfd59ab8c4d1a190a33
DebbySpaninks <dspaninks@gmail.com>
2021-04-28 14:41:58 +0200

    shortened codes, add xTicks, yTicks, diff, enjoy, text, numbers

--- svelte-studentdashboard/src/components/routes/BarChart.svelte
+++ svelte-studentdashboard/src/components/routes/BarChart.svelte
@@ -1,86 +1,64 @@
 <!-- Component to display BarChart -->
 
 <script>
+  export let diffScores = [];
+  export let enjoyScores = [];
 
-  export let scores = [
-    { name: 'toets 1',
-      fun: 4,
-      level: 2 },
-    { name: 'toets 2',
-      fun: 5,
-      level: 3 },
-    { name: 'toets 3',
-      fun: 1,
-      level: 5 },
-    { name: 'toets 4',
-      fun: 3,
-      level: 4 },
-    { name: 'toets 5',
-      fun: 4,
-      level: 1 },
-    { name: 'toets 1',
-      fun: 4,
-      level: 2 },
-    { name: 'toets 2',
-      fun: 5,
-      level: 3 },
-    { name: 'toets 3',
-      fun: 1,
-      level: 5 },
-    { name: 'toets 4',
-      fun: 3,
-      level: 4 },
-    { name: 'toets 5',
-      fun: 4,
-      level: 1 },
-    { name: 'toets 1',
-      fun: 4,
-      level: 2 },
-    { name: 'toets 2',
-      fun: 5,
-      level: 3 },
-    { name: 'toets 3',
-      fun: 1,
-      level: 5 },
-    { name: 'toets 4',
-      fun: 3,
-      level: 4 },
-    { name: 'toets 5',
-      fun: 4,
-      level: 1 }
-  ];
+  export let xTicks = [];
+  // console.log(xTicks);
+
+  const yTicks = [5, 4, 3, 2, 1, 0];
+  // console.log(yTicks);
 
   // $: barWidth = 10;
 
 </script>
 <div class="svg-container">
-  <svg viewbox="0 0 900 400" fill="gray">
+  <svg viewbox="0 0 1000 800" fill="#222">
     <!-- Move to 0(x) 130(y), h(x) / v(y) = length  -->
-    <path d="M35 295 h799 v1 H35 M35 20 h1 v275 H35 z"/>
-    <text x="10" y="26">5</text>
-    <text x="10" y="81">4</text>
-    <text x="10" y="135.7">3</text>
-    <text x="10" y="190.5">2</text>
-    <text x="10" y="245">1</text>
-    <text x="10" y="301">0</text>
+    <path d="M35 245 h800 v1 H35 M35 20 h1 v225 H35 z" />
 
-    <path d="M25 20 h20 v1 H25 M0 0 h0 v275 H0 z" />
-    <path d="M25 75 h20 v1 H25 M0 0 h0 v275 H0 z" />
-    <path d="M25 130 h20 v1 H25 M0 0 h0 v275 H0 z" />
-    <path d="M25 185 h20 v1 H25 M0 0 h0 v275 H0 z" />
-    <path d="M25 240 h20 v1 H25 M0 0 h0 v275 H0 z" />
-    <path d="M25 295 h10 v1 H25 M0 0 h0 v275 H0 z" />
-
-    {#each scores as score, i}
-      <path d="M{57 + (i * 55)} 95 h20 v200 H{57 + (i * 55)} z" fill="green" />
-      <path d="M{77 + (i * 55)} 55 h20 v240 H{77 + (i * 55)} z" fill="red" />
+<!-- horizontal pink bar -->
+    {#each diffScores as bar, i}
+      <path d="M{45 + (i * 45)} 112 h14 v133 H{45 + (i * 45)} z" fill="#FF377A" />
   	{/each}
+
+<!-- horizontal green bar -->
+    {#each enjoyScores as bar, i}
+      <path d="M{60 + (i * 45)} 67 h14 v178 H{60 + (i * 45)} z" fill="#00D84F" />
+  	{/each}
+
+<!-- horizontal ticks -->
+    {#each xTicks as tick, i}
+      <path d="M{59 + (i * 45)} 246 h1 v10 H{59 + (i * 45)} M0 0 h0 v275 H0 z" />
+    {/each}
+
+<!-- horizontal text -->
+    {#each xTicks as text, i}
+      {#if text}
+        <text class="text" x={40 + (i * 45)} y="270">{text}</text>
+      {/if}
+    {/each}
+
+<!-- vertical numbers -->
+    {#each yTicks as grade, i}
+      {#if grade <= 5}
+        <text class="grades" x="10" y={26 + (i * 45)}>{grade}</text>
+      {/if}
+    {/each}
+
+<!-- vertical ticks -->
+    {#each yTicks as tick, i}
+      <path d="M25 {20 + (i * 45)} h10 v1 H25 M0 0 h0 v275 H0 z" />
+    {/each}
 
   </svg>
 </div>
 
 <style>
+
   .svg-container {
+    /* 56(assignments) x 20 = 1120 */
     min-width: 1120px;
     overflow: auto;
   }
@@ -88,4 +66,13 @@
   svg {
     /* background-color: #F3F4F5; */
   }
+
+  .text {
+    font-size: 0.5em;
+  }
+
+  .grades {
+    font-size: 0.8em;
+  }
+
 </style>
