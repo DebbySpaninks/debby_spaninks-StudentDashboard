9e4640fe5bc1ed5f820ce7c47564181332e0907c
DebbySpaninks <dspaninks@gmail.com>
2021-04-29 22:10:20 +0200

    make barchart easier to read/view

--- svelte-studentdashboard/src/components/routes/BarChart.svelte
+++ svelte-studentdashboard/src/components/routes/BarChart.svelte
@@ -1,8 +1,8 @@
 <!-- Component to display BarChart -->
 
 <script>
-  export let diffScores = [];
   export let enjoyScores = [];
+  export let diffScores = [];
   export let xTicks = [];
 
   const yTicks = [ 5, 4, 3, 2, 1, 0 ];
@@ -10,57 +10,69 @@
   // $: barWidth = 10;
 
 </script>
+
 <div class="svg-container">
   <div class="svg-wrapper" style="--viewboxWidth:{diffScores.length * 42}px">
-    <svg viewbox="0 0 {diffScores.length * 42} 370" fill="#222">
-      <!-- Move to 0(x) 130(y), h(x) / v(y) = length  -->
-      <path d="M35 245 h800 v1 H35 M35 20 h1 v225 H35 z" />
 
-      <!-- horizontal pink bar -->
-      {#each diffScores as score, i}
-        <path
-          d="M{43 + (i * 43)} {245 - ((225 / 5) * score)} h10 v{(225 / 5) * score} H{43 + (i * 43)} z"
-          fill="#FF377A"
-        />
-    	{/each}
+    <svg viewbox="0 0 {diffScores.length * 42} 370" fill="#222">
+
+      <!-- black baselines (x- and y-axis) -->
+      <!-- Move to 0(x) 130(y), h(x) / v(y) = length  -->
+      <path d="M35 245 h2064 v1 H35 M35 20 h1 v225 H35 z" />
 
       <!-- horizontal green bar -->
       {#each enjoyScores as score, i}
         <path
-          d="M{54 + (i * 43)} {245 - ((225 / 5) * score)} h10 v{(225 / 5) * score} H{54 + (i * 43)} z"
+          d="M{43 + (i * 37)} {245 - ((225 / 5) * score)} h10 v{(225 / 5) * score} H{43 + (i * 37)} z"
           fill="#00D84F"
+        />
+    	{/each}
+
+      <!-- horizontal pink bar -->
+      {#each diffScores as score, i}
+        <path
+          d="M{54 + (i * 37)} {245 - ((225 / 5) * score)} h10 v{(225 / 5) * score} H{54 + (i * 37)} z"
+          fill="#FF377A"
         />
     	{/each}
 
       <!-- horizontal ticks -->
       {#each xTicks as tick, i}
-        <path d="M{53 + (i * 43)} 246 h1 v10 H{53 + (i * 43)} M0 0 h0 v275 H0 z" />
+        <path d="M{53 + (i * 37)} 246 h1 v10 H{53 + (i * 37)} M0 0 h0 v275 H0 z" />
       {/each}
 
       <!-- horizontal text -->
       {#each xTicks as text, i}
         {#if text}
           <text
-            class="text" x={59 + (i * 43)} y="265"
-            transform="rotate(-45 {59 + (i * 43)} 270)"
+            class="text" x={60 + (i * 37)} y="265"
+            transform="rotate(-45 {60 + (i * 37)} 270)"
             style="text-anchor: end">{text}
           </text>
         {/if}
       {/each}
 
       <!-- vertical numbers -->
-      {#each yTicks as grade, i}
+      <!-- {#each yTicks as grade, i}
         {#if grade <= 5}
           <text class="grades" x="10" y={26 + (i * 45)}>{grade}</text>
         {/if}
-      {/each}
+      {/each} -->
 
-      <!-- vertical ticks -->
+      <!-- vertical ticks and dashed lines -->
       {#each yTicks as tick, i}
         <path d="M25 {20 + (i * 45)} h10 v1 H25 M0 0 h0 v275 H0 z" />
+        <path d="M38 {20.5 + (i * 45)} L 2099 {20.5 + (i * 45)}"
+              stroke="black"
+              stroke-linecap="round"
+              stroke-width="0.2"
+              stroke-dasharray="2,2"
+              fill="none"
+        />
       {/each}
 
-  </svg>
+    </svg>
+
   </div>
 </div>
 
@@ -85,6 +97,8 @@
 
   .grades {
     font-size: 0.8em;
+    background-color: #F3F4F5;
+
   }
 
 </style>
