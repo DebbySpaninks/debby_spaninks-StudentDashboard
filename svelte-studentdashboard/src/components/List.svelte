1e57902f0daf97ae39c3ae2c00fcb5dcda0caded
DebbySpaninks <dspaninks@gmail.com>
2021-04-29 16:43:58 +0200

    add Link

--- svelte-studentdashboard/src/components/List.svelte
+++ svelte-studentdashboard/src/components/List.svelte
@@ -1,28 +1,20 @@
 <!-- Component to display List -->
 
 <script>
-  export let items = [];
+import { Link } from 'svelte-navigator';
 
-items = Array.apply(null, { length: 10 }).map((_, i) => `item ${++i}`);
-
-function click(e) {
-    const li = e.target.closest('li');
-    if (li) {
-      const item = items[+li.value];
-      console.log('Clicked', item);
-    }
-}
+export let items = [];
 
 </script>
 
-  <ul on:click={click}>
-  {#each items as item, i}
-    <li value={i}>{item}</li>
-
-  {:else}
-    <h3>Helaas geen studenten aanwezig</h3>
-  {/each}
-</ul>
+  <ul>
+    {#each items as item, i}
+      <li>
+        <Link to="/staafdiagram/{item}">{item}</Link></li>
+    {:else}
+      <h3>Helaas geen studenten aanwezig</h3>
+    {/each}
+  </ul>
 
 <style>
   ul {
