6882a0f2bc7c3b11456628983f2363110a06684c
DebbySpaninks <dspaninks@gmail.com>
2021-04-30 10:01:29 +0200

    test to make yNumbers sticky

--- svelte-studentdashboard/src/components/List.svelte
+++ svelte-studentdashboard/src/components/List.svelte
@@ -4,7 +4,7 @@
 import { Link } from 'svelte-navigator';
 
 export let items = [];
-const yTicks = [ 5, 4, 3, 2, 1, 0 ];
+// const yTicks = [ 5, 4, 3, 2, 1, 0 ];
 
 
 </script>
@@ -21,17 +21,36 @@ const yTicks = [ 5, 4, 3, 2, 1, 0 ];
 <svg>
 
   <!-- vertical numbers -->
-  {#each yTicks as grade, i}
+  <!-- {#each yTicks as grade, i}
     {#if grade <= 5}
-      <text class="grades" x="10" y={26 + (i * 45)}>{grade}</text>
+      <text class="grades" x="2" y={26 + (i * 45)}>{grade}</text>
     {/if}
-  {/each}
+  {/each} -->
+
+  <!-- vertical ticks and dashed lines -->
+  <!-- {#each yTicks as tick, i}
+    <path d="M17 {20 + (i * 45)} h10 v1 H17 M0 0 h0 v275 H0 z" />
+    <path d="M38 {20.5 + (i * 45)} L 2099 {20.5 + (i * 45)}"
+          stroke="black"
+          stroke-linecap="round"
+          stroke-width="0.2"
+          stroke-dasharray="2,2"
+          fill="none"
+    />
+  {/each} -->
 
 </svg>
 
 <style>
+
   ul {
     list-style-type: none;
     height: 19.7em;
+    padding: 3.3em 3.5em 3em;
   }
+
+  svg {
+    width: 2em;
+  }
+
 </style>
