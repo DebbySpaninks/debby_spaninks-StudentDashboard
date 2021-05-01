45d3923b2593b14daa0cc552fd09014e3e2a79d0
DebbySpaninks <dspaninks@gmail.com>
2021-05-01 11:17:50 +0200

    update styling

--- svelte-studentdashboard/src/components/List.svelte
+++ svelte-studentdashboard/src/components/List.svelte
@@ -1,30 +1,27 @@
 <!-- Component to display List -->
-
 <script>
-import { Link } from 'svelte-navigator';
+  import { Link } from "svelte-navigator";
 
-export let items = [];
-
-
+  export let items = [];
 </script>
 
-  <ul>
-    {#each items as item, i}
-      <li>
-        <Link to="/staafdiagram/{item}">{item}</Link></li>
-    {:else}
-      <h3>Helaas geen studenten aanwezig</h3>
-    {/each}
-  </ul>
+<ul>
+  {#each items as item, i}
+    <li>
+      <Link to="/staafdiagram/{item}">{item}</Link>
+    </li>
+  {:else}
+    <h3>Helaas geen studenten aanwezig</h3>
+  {/each}
+</ul>
 
 <style>
-
   ul {
-    display: flex;
-    flex-direction: column;
     list-style-type: none;
-    height: 19.7em;
-    padding: 3.3em 3em 3em 3.5em;
+    order: 0;
+    padding: 2em;
+    overflow: auto;
+    flex-shrink: 0;
+    margin: 0;
   }
-
 </style>
