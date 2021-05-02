94fdecccb70c6836927fbe1377720ad4b623fc87
DebbySpaninks <dspaninks@gmail.com>
2021-05-02 21:11:38 +0200

    delete home-btn and change title to ""

--- svelte-studentdashboard/src/components/Header.svelte
+++ svelte-studentdashboard/src/components/Header.svelte
@@ -2,21 +2,15 @@
 <script>
   import { Link } from "svelte-navigator";
 
-  export let title = "Staafdiagram";
+  export let title = "";
 </script>
 
 <div class="header">
   <nav>
     <h1>{title}</h1>
-
     <Link to="/">
-      <button>Home</button>
-    </Link>
-
-    <Link to="/staafdiagram">
       <button>Staafdiagram</button>
     </Link>
-
     <Link to="/tabeloverzicht">
       <button>Tabel-overzicht</button>
     </Link>
@@ -26,10 +20,10 @@
 <style>
   div.header {
     margin: 0;
-    padding: 2em;
-    height: 180px;
+    padding: 1.5em;
+    height: 155px;
     box-sizing: border-box;
-    background: #fcf;
+    background: linear-gradient(to bottom, #30DA6E 50%, #FF377A 80%);
   }
 
   nav {
@@ -37,13 +31,14 @@
   }
 
   h1 {
-    margin: 0em 0em 0.4em;
+    margin: 0em 0em 0.2em;
     font-size: 3em;
     outline: none;
   }
 
   button {
-    /* order: 1; */
     white-space: nowrap;
+    border-radius: 3px;
+    padding-top: 0.35em;
   }
 </style>
