c0bd54ca28e0f5cbf1cd5864861de862ecbe949a
DebbySpaninks <dspaninks@gmail.com>
2021-05-07 18:26:47 +0200

    create function to display average

--- svelte-studentdashboard/src/App.svelte
+++ svelte-studentdashboard/src/App.svelte
@@ -18,17 +18,47 @@
   // array of all studentnames (studentdata.map(row => row[0])
   const uniqueItems = index =>
     [ ...new Set(studentdata.map(row => row[index])) ];
-  const extractCol = index => studentdata.map(row => row[index]);
 
   $: studentnames = studentdata ? uniqueItems(0) : [];
-  $: assignments = studentdata ? uniqueItems(1) : [];
-  $: enjoymentRating = studentdata ? extractCol(2) : [];
-  $: difficultyRating = studentdata ? extractCol(3) : [];
-  // $: assignments && console.log(assignments.length);
 
-const rating = (index, array) =>
-    enjoymentRating.slice(index * assignments.length, (index + 1) * assignments.length);
-  // console.log(rating(0, enjoymentRating));
+  // function to display averages from all students or single student
+  function ratings(studentdata, studentname) {
+    return studentdata.reduce((averages, row) => {
+      if (studentname && (row[0] !== studentname)) {
+        return averages;
+      }
+      const key = row[1];
+      const enjoymentRating = parseInt(row[2]) || null;
+      const difficultyRating = parseInt(row[3]) || null;
+      if (!averages[key]) {
+        averages[key] = {
+          enjoymentRatings: [],
+          difficultyRatings: [],
+          enjoymentAverage: 0,
+          difficultyAverage: 0
+        };
+      }
+
+      if (enjoymentRating !== null) {
+        averages[key].enjoymentRatings.push(enjoymentRating);
+        // recalculate average if a new rating is added
+        averages[key].enjoymentAverage = averages[key].enjoymentRatings.reduce((num, rating) => (num += rating), 0) / averages[key].enjoymentRatings.length;
+      }
+
+      if (difficultyRating !== null) {
+        averages[key].difficultyRatings.push(difficultyRating);
+        // recalculate average if a new rating is added
+        averages[key].difficultyAverage = averages[key].difficultyRatings.reduce((num, rating) => (num += rating), 0) / averages[key].difficultyRatings.length;
+      }
+
+      return averages;
+    }, {});
+  }
+
+  const enjoymentRating = ratings => Object.values(ratings).map(assignment => assignment.enjoymentAverage);
+  const difficultyRating = ratings => Object.values(ratings).map(assignment => assignment.difficultyAverage);
+  const assignments = ratings => Object.keys(ratings);
+
 
   let sizes = {};
 
@@ -58,9 +88,9 @@ const rating = (index, array) =>
           <List items={studentnames.sort()} />
           <div class="chart">
             <BarChart
-              bind:xTicks={assignments}
-              bind:enjoyScores={enjoymentRating}
-              bind:diffScores={difficultyRating}
+              xTicks={assignments(ratings(studentdata))}
+              enjoyScores={enjoymentRating(ratings(studentdata))}
+              diffScores={difficultyRating(ratings(studentdata))}
             />
           </div>
         {/if}
@@ -76,9 +106,9 @@ const rating = (index, array) =>
         <List items={studentnames.sort()} />
         <div class="chart">
           <BarChart
-            bind:xTicks={assignments}
-            bind:enjoyScores={enjoymentRating}
-            bind:diffScores={difficultyRating}
+            xTicks={assignments(ratings(studentdata, params.id))}
+            enjoyScores={enjoymentRating(ratings(studentdata, params.id))}
+            diffScores={difficultyRating(ratings(studentdata, params.id))}
           />
         </div>
       {/if}
